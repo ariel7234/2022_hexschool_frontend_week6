0aecd623b13e53d996e36b59efbba8a4d32f42f1
YJ Chen <ariel7234@gmail.com>
2022-09-14 03:09:10 +0800

    .

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,28 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+//FIXME: swiper 無法自訂每頁卡片數量
+
+// var cardSwiper = new Swiper(".swiper-card", {
+
+//   loop: true,
+//   autoplay: {
+//     delay: 2500,
+//     // disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     // 375: {
+//     //   slidesPerView: 3,
+//     //   spaceBetween: 8,
+//     // },
+//     // 640: {
+//     //   slidesPerView: 2,
+//     //   spaceBetween: 20,
+//     // },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 8,
+//     },
+//     // 992: {
+//     //   slidesPerView: 4,
+//     //   spaceBetween: 24,
+//     // },
+//   },
+// });
