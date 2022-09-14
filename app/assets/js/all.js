c7f80bb8ecc5c49657ad47346d02f8ef62b38213
YJ Chen <ariel7234@gmail.com>
2022-09-14 23:45:35 +0800

    .

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -26,3 +26,17 @@
 //     // },
 //   },
 // });
+
+var shareSwiper = new Swiper(".swiper-custom", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
